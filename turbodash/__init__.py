a0cd334b6bae14911a855f4d15be2bc7ad87162c
roagr <roagr@dtu.dk>
2025-10-25 10:37:52 +0200

    Bump version: 0.1.1 → 0.1.2

--- turbodash/__init__.py
+++ turbodash/__init__.py
@@ -15,7 +15,7 @@ from .graphics import *
 
 
 # Package info
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 PACKAGE_NAME = "turbodash"
 URL_GITHUB = "https://github.com/turbo-sim/turbodash"
 # URL_DOCS = "https://turbo-sim.github.io/turbodash/"
